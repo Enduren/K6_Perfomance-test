7a9a7a0e776bdab27a80d5c5aed5b5d52483b899
dexter <dtennison79@gmail.com>
2026-06-06 11:32:25 -0500

    add new test

--- scripts/first_k6_scripts.js
+++ scripts/first_k6_scripts.js
@@ -1,24 +1,41 @@
+// Import the http module from the k6 library, which is used to make HTTP requests in the test.+
 import http from 'k6/http';
-import { sleep } from 'k6';
+
+// Import the sleep function from the k6 library, which is used to pause the execution of the test for a specified amount of time.
+import { sleep,check } from 'k6';
+
+// Import the htmlReport function from the k6-reporter library, which is used to generate an HTML report from the test data.
 import {htmlReport} from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 
 // This function is called at the beginning of the test, and it sets up the test configuration.
 export const options = {
     vus: 10, // Number of virtual users
-    duration: '30s', // Duration of the test
+    duration: '10s', // Duration of the test
+
+    thresholds: {
+        http_req_duration: ['p(95)<500'], // 95% of requests should complete within 500ms
+    },
 };
 
 // This function is called for each virtual user, and it performs the actual test actions.
 
 export default function () {
-    http.get('https://test.k6.io');
+    // Make an HTTP GET request to the specified URL and store the response in a variable called response.
+    const response= http.get('https://test.k6.io');
+    
+    // Check if the response status is 200, which indicates that the request was successful. If the check fails, it will be recorded in the test results.
+    check(response, {        
+        'is status 200': (r) => r.status === 200,
+    });
     sleep(1);
 }
 
 // This function is called at the end of the test, and it generates an HTML report from the test data.
-export function handleSummary(data) {
-    return {
-        "file.html": htmlReport(data)
-    };
-}
+// export function handleSummary(data) {
+//     return {
+//         "file.html": htmlReport(data)
+//     };
+// }
+
+// k6 run scripts/first_k6_scripts.js   
